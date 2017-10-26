439082c0ab862f7b284a9e2c0e11e876e08f70d9
savageno2@hotmail.com <111222gmm>
2017-10-26 17:52:19 +0800

    1

--- models/database.js
+++ models/database.js
@@ -1,6 +1,26 @@
 const pg = require('pg');
-const connectionstring = process.env.DATABASE_URL || 'postgres://139.162.84.23:5432/wechat_public_account';
+const connectionstring = 'postgres://139.162.84.23:5432/wechat_public_account';
 
-const client = new pg.client(connectionstring);
-client.connect();
-const query = client.query('');
+const client = new pg.Client(connectionstring);
+client.connect(
+	function(err){
+		if(err){
+			return console.error('could not connect to postgres',err);
+		};
+
+		//drop existing table
+		console.log('Dropping table \'person\'');
+		var query = client.query('drop table if exists person');
+		query.on('end',function(){
+			console.log('Dropped!');
+		})
+
+		//create table
+		console.log('Creating table \'person\'');
+		var query = client.query('create table person(id serial, username varchar(10), password varchar(10), age integer)');
+		query.on('end',function(){
+			console.log('Created!');
+		})
+
+	}
+);
