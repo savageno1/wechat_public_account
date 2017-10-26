2ba31061c6a1d17c663df9f3b11b40fe617d54d6
savageno2@hotmail.com <111222gmm>
2017-10-26 17:38:32 +0800

    Initial commit

---
+++ models/database.js
@@ -0,0 +1,7 @@
+const pg = require('pg');
+const connectionstring = process.env.DATABASE_URL || 'postgres://139.162.84.23:5432/wechat_public_account';
+
+const client = new pg.client(connectionstring);
+client.connect();
+const query = client.query('');
+query.on('end',() => { client.end(); });
